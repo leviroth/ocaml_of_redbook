7204651da27f1a96c520bb72505b875af92945ee
Levi Roth <levimroth@gmail.com>
2018-04-29 23:43:58 -0400

    Add Make functor to Monad module

--- main.ml
+++ main.ml
@@ -1,14 +1,31 @@
-module type MONAD = sig
-  type 'a t
-  val return : 'a -> 'a t
-  val (>>=) : 'a t -> ('a -> 'b t) -> 'b t
-  val (>>|) : 'a t -> ('a -> 'b) -> 'b t
-  val map2 : 'a t -> 'b t -> f:('a -> 'b -> 'c) -> 'c t
+module Monad = struct
+  module type Base = sig
+    type 'a t
+    val return : 'a -> 'a t
+    val (>>=) : 'a t -> ('a -> 'b t) -> 'b t
+  end
+
+  module type S = sig
+    include Base
+    val (>>|) : 'a t -> ('a -> 'b) -> 'b t
+    val map2 : 'a t -> 'b t -> f:('a -> 'b -> 'c) -> 'c t
+  end
+
+  module Make (B : Base) : S with type 'a t := 'a B.t = struct
+    include B
+
+    let (>>|) m f = m >>= fun x -> return (f x)
+
+    let map2 m1 m2 ~f =
+      m1 >>= fun a ->
+      m2 >>| fun b ->
+      f a b
+  end
 end
 
 module type STATE = sig
   type state
-  include MONAD with type 'a t = state -> state * 'a
+  include Monad.S with type 'a t = state -> state * 'a
   val get : state t
   val put : state -> unit t
   val run : 'a t -> init:state -> state * 'a
@@ -17,21 +34,16 @@ end
 module State (S : sig type t end)
   : STATE with type state = S.t = struct
   type state = S.t
-  type 'a t = state -> state * 'a
-  let return v s = (s, v)
-  let (>>=) m k s = let s', a = m s in k a s'
-  let (>>|) m k s = let s', a = m s in (s', k a)
+  module Monad_base : Monad.Base with type 'a t = state -> state * 'a = struct
+    type 'a t = state -> state * 'a
+    let return v s = (s, v)
+    let (>>=) m k s = let s', a = m s in k a s'
+  end
+  include Monad_base
+  include Monad.Make(Monad_base)
   let get s = (s, s)
   let put s _ = (s, ())
   let run m ~init = m init
-  let map2 m1 m2 ~f s =
-    let s', a = m1 s in
-    let s'', b = m2 s' in
-    s'', f a b
-  let map2 m1 m2 ~f =
-    m1 >>= fun a ->
-    m2 >>| fun b ->
-    f a b
 end
 
 module RNG : sig
